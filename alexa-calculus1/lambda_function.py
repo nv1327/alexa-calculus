0e6b6b47c64c41a71e82e82e3ecb4e14520be8f3
Nick Vitebsky <n.vitebsky1327@gmail.com>
2017-12-09 23:43:38 -0500

    ACTUALLY WORKS

--- alexa-calculus1/lambda_function.py
+++ alexa-calculus1/lambda_function.py
@@ -4,23 +4,32 @@ import json
 client = wolframalpha.Client("AAREUR-UVEWA5PATW")
 
 def main(inputstatement):
-    res = client.query(inputstatement)
     if (inputstatement.lower().find("indefinite integral")!=-1):
+        inputstatement = inputstatement[inputstatement.lower().find("indefinite integral"):]
+        res = client.query(inputstatement)
         answer = res.details["Indefinite integral"]
     elif (inputstatement.lower().find("definite integral")!=-1):
+        inputstatement = inputstatement[inputstatement.lower().find("definite integral"):]
+        res = client.query(inputstatement)
         answer = res.details["Definite integral"]
     elif (inputstatement.lower().find("derivative")!=-1):
+        inputstatement = inputstatement[inputstatement.lower().find("derivative"):]
+        res = client.query(inputstatement)
         answer = res.details["Derivative"]
     elif (inputstatement.lower().find("taylor series")!= -1):
-        answer = "Taylor series functionality is not yet implemented"
+        answer = "Taylor series functionality has not currently been implemented"
     elif (inputstatement.lower().find("limit") != -1):
-        answer = res.details["Limit"]
+        inputstatement = inputstatement[inputstatement.lower().find("limit"):]
+        res = client.query(inputstatement)
+        answer = res.details["limit"]
     else:
-        answer = res.details["Result"]
+        answer = "Invalid calculation"
+
     return answer
 
 def lambda_handler(event, context):
-    print(main("Derivative of 2x"))
-    print(main(event['request']['intent']['slots']['query']['value']))
+    output = main(event['request']['intent']['slots']['query']['value'])
+
+    return {"version":"1.0","response":{"outputSpeech":{"type":"PlainText","text":output}},"sessionAttributes":{}}
 
 #we need to put the key ['value'] into main.main() but parse for a different json key as output
